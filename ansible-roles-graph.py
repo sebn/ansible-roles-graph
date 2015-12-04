36ec5864e8981bd8f103519ff852d6c580a8626b
Sebastien Nicouleaud <sebn@users.noreply.github.com>
2015-12-04 18:25:13 +0100

    Parse command-line arguments

--- ansible-roles-graph.py
+++ ansible-roles-graph.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
 
-import sys
+from argparse import ArgumentParser
+from os.path import join
 import gv
 from glob import glob
 import yaml
 
-g = gv.digraph('roles')
+p = ArgumentParser(description='Generate a picture of ansible roles graph.')
 
+p.add_argument('roles_dirs',
+               metavar='ROLES_DIR',
+               type=str,
+               nargs='+',
+               default='./roles/',
+               help='a directory containing ansible roles')
+
+p.add_argument('-o', '--output',
+               type=str,
+               default='./ansible-roles.png',
+               help='the output file')
+
+p.add_argument('-f', '--format',
+               type=str,
+               default='png')
+
+args = p.parse_args()
+g = gv.digraph('roles')
 role_nodes = {}
 
 def add_role(role):
@@ -19,17 +38,18 @@ def link_roles(dependent, depended):
         role_nodes[depended_role]
     )
 
-for path in glob('roles/*/meta/main.yml'):
-    dependent_role = path.split('/')[1]
+for roles_dir in args.roles_dirs:
+    for path in glob(join(roles_dir, '*/meta/main.yml')):
+        dependent_role = path.split('/')[-3]
 
-    add_role(dependent_role)
+        add_role(dependent_role)
 
-    with open(path, 'r') as f:
-        for dependency in yaml.load(f.read())['dependencies']:
-            depended_role = dependency['role']
+        with open(path, 'r') as f:
+            for dependency in yaml.load(f.read())['dependencies']:
+                depended_role = dependency['role']
 
-            add_role(depended_role)
-            link_roles(dependent_role, depended_role)
+                add_role(depended_role)
+                link_roles(dependent_role, depended_role)
 
 gv.layout(g, 'dot')
-gv.render(g, 'png', 'ansible-roles.png')
+gv.render(g, args.format, args.output)
