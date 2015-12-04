c03ad79f52c076a4b117c4a52b0e91dd00002918
Sebastien Nicouleaud <sebn@users.noreply.github.com>
2015-12-04 20:56:58 +0100

    Introduce GraphBuilder

--- ansible-roles-graph.py
+++ ansible-roles-graph.py
@@ -25,31 +25,36 @@ p.add_argument('-f', '--format',
                default='png')
 
 args = p.parse_args()
-g = gv.digraph('roles')
-role_nodes = {}
 
-def add_role(role):
-    if role not in role_nodes:
-        role_nodes[role] = gv.node(g, role)
+class GraphBuilder:
+    def __init__(self):
+        self.g = gv.digraph('roles')
+        self.role_nodes = {}
 
-def link_roles(dependent, depended):
-    gv.edge(
-        role_nodes[dependent_role],
-        role_nodes[depended_role]
-    )
+    def add_role(self, role):
+        if role not in self.role_nodes:
+            self.role_nodes[role] = gv.node(self.g, role)
+
+    def link_roles(self, dependent, depended):
+        gv.edge(
+            self.role_nodes[dependent_role],
+            self.role_nodes[depended_role]
+        )
+
+builder = GraphBuilder()
 
 for roles_dir in args.roles_dirs:
     for path in glob(join(roles_dir, '*/meta/main.yml')):
         dependent_role = path.split('/')[-3]
 
-        add_role(dependent_role)
+        builder.add_role(dependent_role)
 
         with open(path, 'r') as f:
             for dependency in yaml.load(f.read())['dependencies']:
                 depended_role = dependency['role']
 
-                add_role(depended_role)
-                link_roles(dependent_role, depended_role)
+                builder.add_role(depended_role)
+                builder.link_roles(dependent_role, depended_role)
 
-gv.layout(g, 'dot')
-gv.render(g, args.format, args.output)
+gv.layout(builder.g, 'dot')
+gv.render(builder.g, args.format, args.output)
